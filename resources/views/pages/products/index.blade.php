ec5cc3d69d250ce0aee7a763c593b03b4a33a5a3
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2025-01-08 13:14:15 +0700

    refactor: split AdminLTE HTML into separate components

---
+++ resources/views/pages/products/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('header')
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1>Dashboard</h1>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Widgets</li>
+            </ol>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    View Products section
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.main');
 });
+
+Route::get('/products', function () {
+    return view('pages.products.index');
+});
